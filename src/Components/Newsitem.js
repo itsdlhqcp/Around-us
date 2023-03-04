9a409e4e4b04cd4d68d0cb08dd1658f9f3abfbf9
itsdlhqcp <dilhaqued@gmail.com>
2023-03-04 14:30:27 +0530

    TOPICS ADDED 1

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -4,7 +4,7 @@ const Newsitem =(props)=>{
     let {title,description,imageUrl,key}=props;    //newsUrl,author,date,
     return (
       <div key={key} className='my-3'>
-          <div className="card" style={{width: "25rem"}}>
+          <div className="card" style={{width: "22rem"}}>
               <img src={imageUrl} className="card-img-top" alt="..."/>
               <div className="card-body">
                 <h5 className="card-title">{title}</h5>
