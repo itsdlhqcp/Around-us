8fc58f742a5ea0df7e286a05886b034502574806
itsdlhqcp <dilhaqued@gmail.com>
2023-03-26 18:26:40 +0530

    active article

--- src/Components/VoiceAssist/VoiceScreen.js
+++ src/Components/VoiceAssist/VoiceScreen.js
@@ -3,14 +3,12 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import NewsCards from './NewsCards/NewsCards';
-// import './SplashScreen.css'; // Import the CSS file for styling
-import useStyles from '../styles';
 import wordsToNumbers from 'words-to-numbers';
 
 function VoiceScreen() {
     const [newsArticles, setNewsArticles] = useState([]);
     const [activeArticle, setActiveArticle] = useState(-1);
-    const classes = useStyles();
+    
     useEffect(() => {
         alanBtn({
             key: '190254eb16a8a0645ea8924c3f109dda2e956eca572e1d8b807a3e2338fdd0dc/stage',
@@ -35,15 +33,9 @@ function VoiceScreen() {
     }, [])
   return (
     <div style={{ marginBottom: '3rem' }}>
-    <div className={classes.logoContainer}>
-        {/* <img src={require('./image/logo.png')} className={classes.alanLogo} alt="News App logo"/> */}
-        {/* <Typography variant='h4'>Conversational Voice</Typography> */}
-    </div>
     <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     <p>@Dilhaque 2023</p>
-    
-    
-</div>
+    </div>
   );
 }
 
