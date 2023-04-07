faf03ff772363e1ab1e04c73eaa2b1373f5e2e58
itsdlhqcp <dilhaqued@gmail.com>
2023-04-08 01:21:19 +0530

    splash screen ui updated 2

--- src/Components/SplashScreen.js
+++ src/Components/SplashScreen.js
@@ -4,19 +4,11 @@ import logo from './assets/clipart68616.png'; // Import the CSS file for styling
 
 function SplashScreen() {
   return (
-    // <div className="splash-screen">
-    //   {/* <img src="splash-image.jpg" alt="Splash Screen Image" /> */}
-    //   <h1>Welcome to our newsapp project!</h1>
-    //   <p>Loading...</p>
-    // </div>
     <>
-    
     <div className="logo-wrapper">
-    <img src={logo} alt="Logo" className="logo" width="165" height="165"/>
+    <img src={logo} alt="Logo" className="logo" width="175" height="175"/>
     </div>
     <h1 style={{ color: 'red', fontFamily: 'italic' }}>Hot Headlines</h1>
-
-
     <div className="loading-wrapper">
     <div className="loading-dot" />
     <div className="loading-dot" />
