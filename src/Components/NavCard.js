b992a81a36359ad06d9d779b41103d1409425f0c
itsdlhqcp <dilhaqued@gmail.com>
2023-04-10 14:33:15 +0530

    voice screen cards ui

--- src/Components/NavCard.js
+++ src/Components/NavCard.js
@@ -1,22 +1,23 @@
 import { Container, Carousel } from 'react-bootstrap';
 
-
 const infoCards = [
-  { color: '#808080', title: 'Get World News' },
-  { color: '#808080', title: 'Get News from India' },
-  { color: '#808080', title: 'Get National News' },
+  { color: '#808080', title: 'Get World News', link: '/world' },
+  { color: '#808080', title: 'Get News from India', link: '/india' },
+  { color: '#808080', title: 'Get National News', link: '/national' },
 ];
 
 const NavCard = () => {
     return (
-      <div style={{ margin: "7px 0px 0px 0px" }}>
+      <div style={{ margin: "5px 0px 0px 0px" }}>
         <Container>
-          <Carousel interval={3000} pause={false} controls={false} indicators={false}>
+          <Carousel interval={2200} pause={false} controls={false} indicators={false}>
             {infoCards.map((infoCard, i) => (
               <Carousel.Item key={i}>
-                <div className="p-1" style={{ backgroundColor: infoCard.color }}>
-                  <h4 style={{ margin: 'auto' }}>{infoCard.title}</h4>
-                </div>
+                <a href={infoCard.link}style={{ textDecoration: 'none' }}>
+                  <div className="p-1" style={{ backgroundColor: infoCard.color}}>
+                    <h4 style={{ margin: 'auto', fontStyle: 'helvetica', color: '#fff' }}>{infoCard.title}</h4>
+                  </div>
+                </a>
               </Carousel.Item>
             ))}
           </Carousel>
@@ -25,6 +26,69 @@ const NavCard = () => {
     );
 };
 
-
-
 export default NavCard;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Container, Carousel } from 'react-bootstrap';
+
+
+// const infoCards = [
+//   { color: '#808080', title: 'Get World News' },
+//   { color: '#808080', title: 'Get News from India' },
+//   { color: '#808080', title: 'Get National News' },
+// ];
+
+// const NavCard = () => {
+//     return (
+//       <div style={{ margin: "7px 0px 0px 0px" }}>
+//         <Container>
+//           <Carousel interval={3000} pause={false} controls={false} indicators={false}>
+//             {infoCards.map((infoCard, i) => (
+//               <Carousel.Item key={i}>
+//                 <div className="p-1" style={{ backgroundColor: infoCard.color }}>
+//                   <h4 style={{ margin: 'auto' }}>{infoCard.title}</h4>
+//                 </div>
+//                 <a/>
+//               </Carousel.Item>
+//             ))}
+//           </Carousel>
+//         </Container>
+//       </div>
+//     );
+// };
+
+
+
+// export default NavCard;
