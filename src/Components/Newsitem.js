4281abf15dfacf6ba7ca3d4694b94cd36d96a603
itsdlhqcp <dilhaqued@gmail.com>
2023-04-25 22:31:49 +0530

    colors ui added to info cards

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -1,40 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Newsitem =(props)=>{
+const Newsitem = (props) => {
+  const { title, description, imageUrl, redMore, date, key } = props;
+  const [showDescription, setShowDescription] = useState(false);
 
-  const handleShare = () => {      //it handle share at web view not in android
-  if (navigator.share) {
-    navigator.share({
-      title: props.title,
-      text: props.description,
-      url: props.redMore,
-    })
-    .then(() => console.log('Link shared successfully.'))
-    .catch((error) => console.log('Error sharing link:', error));
-  } else {
-    console.log('Web Share API not supported.');
-  }
-}
-    let {title,description,imageUrl,key,redMore,date}=props;    //newsUrl,author,date,readMoreUrl
-    return (
-      <div key={key} className='my-3'>
-          <div className="card" style={{width: "21rem"}}>
-              <img src={imageUrl} className="card-img-top" alt="..." sizes="(max-height: 576px) 100vw, (max-height: 768px) 50vw, 33vw" style={{height: "17rem" ,  objectFit: "cover"}}/>
-              <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
-                <p className="card-text"><small className="text-muted">{date}</small></p>
-                <div>
-                <a rel="noreferrer" href={redMore} target="_balnk" className="btn btn-sm btn-dark pr-3">Read More</a>
-                 <button className="btn btn-outline-primary btn-sm" onClick={handleShare} style={{ marginLeft: '10px' }}>
-                 share
-                  </button>
-                </div>
-              </div>
-          </div>
+  const handleReadMore = () => {
+    setShowDescription(!showDescription);
+  };
+
+  const handleShare = () => {
+    if (navigator.share) {
+      navigator
+        .share({
+          title: title,
+          text: description,
+          url: redMore,
+        })
+        .then(() => console.log('Link shared successfully.'))
+        .catch((error) => console.log('Error sharing link:', error));
+    } else {
+      console.log('Web Share API not supported.');
+    }
+  };
+
+  return (
+    <div key={key} className='my-3'>
+      <div className='card' style={{ width: '21rem' }}>
+        <img
+          src={imageUrl}
+          className='card-img-top'
+          alt='...'
+          sizes='(max-height: 576px) 100vw, (max-height: 768px) 50vw, 33vw'
+          style={{ height: '17rem', objectFit: 'cover' }}
+        />
+        <div className='card-body'>
+          <h5 className='card-title'>{title}</h5>
+          {showDescription && <p className='card-text'>{description}</p>}
+          <p className='card-text'>
+            <small className='text-muted'>{date}</small>
+          </p>
+          <button
+            className='btn btn-sm btn-dark'
+            onClick={handleReadMore}
+          >
+            {showDescription ? 'Read Less' : 'Read More'}
+          </button>
+          <a
+            rel='noreferrer'
+            href={redMore}
+            target='_blank'
+            className='btn btn-sm btn-dark ms-2'
+          >
+            Read Full Article
+          </a>
+          <button
+            className='btn btn-outline-primary btn-sm ms-2'
+            onClick={handleShare}
+          >
+            Share
+          </button>
+        </div>
       </div>
-    )
-  
-}
+    </div>
+  );
+};
 
-export default Newsitem
+export default Newsitem;
