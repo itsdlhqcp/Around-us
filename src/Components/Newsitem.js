c825cac0982a0411a79f25c5cc05b5af014110e5
itsdlhqcp <dilhaqued@gmail.com>
2023-04-26 00:44:11 +0530

    BADGE URL

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import'../index.css';
 
 const Newsitem = (props) => {
   const { title, description, imageUrl, redMore, date, key } = props;
   const [showDescription, setShowDescription] = useState(false);
+  const [source, setSource] = useState("");
 
   const handleReadMore = () => {
     setShowDescription(!showDescription);
   };
+
+  useEffect(() => {
+    try {
+      const url = new URL(redMore);
+      setSource(url.hostname);
+    } catch (error) {
+      console.log('Error parsing URL:', error);
+      setSource("Unknown");
+    }
+  }, [redMore]);
 
   const handleShare = () => {
     if (navigator.share) {
@@ -27,6 +38,19 @@ const Newsitem = (props) => {
   return (
     <div key={key} className='my-3'>
       <div className='card'>
+        <div>
+        <div
+          style={{
+            display: "flex",
+            position: "absolute",
+            justifyContent: "flex-end",
+            right: "4.1px",
+          }}
+        >
+          <span className="badge bg-danger">
+            Article By - {source ? source : "Unknown"}
+          </span>
+        </div>
         <img
           src={imageUrl}
           className='card-img-top'
@@ -34,6 +58,8 @@ const Newsitem = (props) => {
           sizes='(max-height: 576px) 100vw, (max-height: 768px) 50vw, 33vw'
           style={{ height: '17rem', objectFit: 'cover' }}
         />
+        </div>
+       
         <div className='card-body'>
           <h5 className='card-title'>{title}</h5>
           {showDescription && <p className='card-text'>{description}</p>}
